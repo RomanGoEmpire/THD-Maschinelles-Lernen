907016ee09b7ed83c443446c8cb19195689b4b2c
Roman Gerloff <71276897+romangoempire@users.noreply.github.com>
2023-03-15 16:45:37 +0100

    added Aufgabe 3 and 4

--- autos.R
+++ autos.R
@@ -13,6 +13,7 @@ data <-
 verbrauch <- data[, "Verbrauch"]
 zylinder <- data[, "Zylinder"]
 hubraum <- data[, "Hubraum"]
+data[, "Herkunft"] <- as.factor(data[, "Herkunft"])
 
 summary(data)
 
@@ -44,9 +45,43 @@ y <- verbrauch
 plot(
   x,
   y,
+  type = "p",
+  main = "Beziehung zwischen Leistung und Verbrauch",
+  
   xlab = "Leistung",
   ylab = "Verbrauch",
   pch = 16,
   col = "blue",
-  cex = 0.7
+  cex = 0.7,
+  
 )
+
+
+
+# Aufgabe 4
+
+mean_by_continent <-
+  aggregate(verbrauch ~ data[, "Herkunft"], data = data, mean)
+names(mean_by_continent) <- c("Herkunft", "Verbrauch")
+mean_by_continent # 1 (=Amerika), 2 (=Europa) und 3 (=Asien)
+
+
+
+# Aufgabe 5
+x <- data[, "Leistung"]
+y <- verbrauch
+group <- table(data$Herkunft)
+
+plot(
+  x,y,
+  type = "p",
+  main = "Beziehung zwischen Leistung und Verbrauch",
+  xlab = "Leistung",
+  ylab = "Verbrauch",
+  
+  pch = 16,
+  factor(group)
+  )
+
+
+
